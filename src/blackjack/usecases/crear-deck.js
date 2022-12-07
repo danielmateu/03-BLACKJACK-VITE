89551fbf82ff81c874fba0ff7f039b4b64fb7120
Daniel Mateu <danielmateu86@gmail.com>
2022-12-07 12:07:18 +0100

    Creacion modulos por defecto e individuales

---
+++ src/blackjack/usecases/crear-deck.js
@@ -0,0 +1,26 @@
+import _ from 'underscore';
+
+
+export const miNombre = 'Dani';
+
+const crearDeck = (tiposDeCarta, tiposEspeciales) => {
+
+    let deck = [];
+
+    for (let i = 2; i <= 10; i++) {
+        for (let tipo of tiposDeCarta) {
+            deck.push(i + tipo);
+        }
+    }
+
+    for (let tipo of tiposDeCarta) {
+        for (let esp of tiposEspeciales) {
+            deck.push(esp + tipo);
+        }
+    }
+    // console.log( deck );
+    deck = _.shuffle(deck);
+    return deck;
+}
+
+export default crearDeck;
